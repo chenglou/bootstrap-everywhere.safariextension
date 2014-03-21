457e5dda67e161d2a2217158b7af552ed7f76563
Cheng Lou <chenglou92@gmail.com>
2014-03-20 23:54:23 -0700

    Bump bs ver, include compiled bin for ppl

--- injectCSS.js
+++ injectCSS.js
@@ -6,7 +6,7 @@ if (window.top === window) {
     var node = document.createElement('link');
     node.setAttribute(
       'href',
-      'http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css'
+      'http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'
     );
     node.setAttribute('rel', 'stylesheet');
     document.head.insertBefore(node, document.head.childNodes[0]);
